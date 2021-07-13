4fbcec72c2b3ad6c2157dc68374c56794e099f1e
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-07-14 00:17:35 +0530

    main class

--- src/Interfaces/Main.java
+++ src/Interfaces/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Interface 1.fxml"));
         primaryStage.setTitle("loging");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 660, 473));
         primaryStage.show();
     }
 
