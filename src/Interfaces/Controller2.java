a83dc18975a9eea66a541afff5bdc106d2eafe6a
sandaru-luckjaya99 <96827@fhss.sjp.ac.lk>
2021-07-27 15:59:30 +0530

    date and time set

--- src/Interfaces/Controller2.java
+++ src/Interfaces/Controller2.java
@@ -1,21 +1,37 @@
 package Interfaces;
 
+import com.sun.media.jfxmediaimpl.platform.Platform;
 import connection.connectioncls;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 
 public class Controller2 implements Initializable {
+
     public TextArea Tax;
+    public DatePicker calender;
+    public Label timer;
+    public Button back;
+    public TextField user;
+
 
     @FXML
     private TableView<table_add_list> tableView;
@@ -30,9 +46,7 @@ public class Controller2 implements Initializable {
     private TableColumn<table_add_list, Integer> quentity_column;
 
     @FXML
-    private TableColumn<table_add_list, Float> price_column;
-
-
+    private TableColumn<table_add_list, Double> price_column;
 
     @FXML
     private Button strt;
@@ -42,7 +56,6 @@ public class Controller2 implements Initializable {
 
     @FXML
     private TextField barcd;
-
 
     @FXML
     private TextField brnd;
@@ -64,14 +77,25 @@ public class Controller2 implements Initializable {
 
     @FXML
     private TextField price;
-
-    Float totalprice = Float.valueOf(0);
-
+    Double totalprice = Double.valueOf(0);
 
 
-    //private Object finanswerg;
 
-    //private finanswerg;
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+
+    public void clock (){
+
+        Date currentTime = new Date();
+        String formatTimestr = "hh:mm:ss";
+        DateFormat formatTime = new SimpleDateFormat(formatTimestr);
+        String formattedTimerStr = formatTime.format(currentTime);
+
+        timer.setText(formattedTimerStr);
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -80,20 +104,34 @@ public class Controller2 implements Initializable {
         quentity_column.setCellValueFactory(new PropertyValueFactory<>("quentity_column"));
         price_column.setCellValueFactory(new PropertyValueFactory<>("price_column"));
         //barcode_column.setCellValueFactory(new PropertyValueFactory<>("Barcode"));
-
-
         tableView.setItems(observableList);
+
+
     }
     ObservableList<table_add_list>observableList = FXCollections.observableArrayList(
 
     );
 
+    public void start(MouseEvent mouseEvent) {
+        clock();
+        LocalDate date = calender.getValue();
+        System.out.println(date);
+
+//        Controller cont = new Controller().con();
+//        //String d = cont.us;
+//
+//        user.setText(cont.us);
+//        System.out.println(cont.us);
+
+
+
+    }
 
 
     public void add(MouseEvent mouseEvent) {
-        Float unitprice = Float.valueOf(unitp.getText());
+        Double unitprice = Double.valueOf(unitp.getText());
         Integer quentity = Integer.valueOf(qty.getText());
-        Float answer = unitprice * quentity;
+        Double answer = unitprice * quentity;
 
         totalprice = totalprice + answer;
         table_add_list table_add_list = new table_add_list(barcd.getText(),nme.getText(), Integer.parseInt(qty.getText()), answer );
@@ -106,45 +144,34 @@ public class Controller2 implements Initializable {
         nme.clear();
         discnt.clear();
 
-
     }
-
-
 
     @FXML
     void addAll(MouseEvent event) {
 
-
-
-
     }
 
 
-
     public void enter_search(MouseEvent mouseEvent) throws SQLException {
-
-
-
 
         connectioncls connectionCls = new connectioncls();
         Connection connection = connectionCls.getConnection();
         try {
             String barcode = barcd.getText();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM instrument WHERE Barcode = ? ");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM instrument_price_list WHERE Barcode = ? ");
             preparedStatement.setString(1,barcode);
             ResultSet instrument_detail = preparedStatement.executeQuery();
+
             if(instrument_detail.next()==true){
                 String names = instrument_detail.getString(2);
                 String brands = instrument_detail.getString(3);
-                String unitprices = instrument_detail.getString(4);
+                Double unitprices = instrument_detail.getDouble(4);
                 String discounts = instrument_detail.getString(5);
 
                 nme.setText(names);
                 brnd.setText(brands);
-                unitp.setText(unitprices);
+                unitp.setText(String.valueOf(unitprices));
                 discnt.setText(discounts);
-
-
 
             }
 
@@ -152,14 +179,40 @@ public class Controller2 implements Initializable {
             e.printStackTrace();
         }
 
-
-        //nme.setText();
-
-
     }
+
 
     public void endtot(MouseEvent mouseEvent) {
 
         price.setText(String.valueOf(totalprice));
+//        tableView.getItems().clear();
+    }
+
+
+    public void clr(MouseEvent mouseEvent) {
+
+        tableView.getItems().clear();
+
+        unitp.clear();
+        barcd.clear();
+        brnd.clear();
+        qty.clear();
+        nme.clear();
+        discnt.clear();
+
+
+    }
+
+
+    public void bck(MouseEvent mouseEvent) throws IOException {
+
+        Stage stage = (Stage) back.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("Interface 1.fxml"));
+        stage.setTitle("Logging");
+        stage.setScene(new Scene(root,600,500));
+
+        stage.show();
+        //stage.setResizable(true);
+
     }
 }
